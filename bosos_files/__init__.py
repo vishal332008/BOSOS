1a74572bc536eb7ff0617882caee073c1b98c1fa
shailesh_gabu_11 <116952795+Shailesh051211@users.noreply.github.com>
2025-03-10 18:24:33 +0530

    Update __init__.py

--- bosos_files/__init__.py
+++ bosos_files/__init__.py
@@ -1,3 +1,18 @@
-from . import home, apps, utils
+# ba_meta require api 9
 
-__all__ = ['home', 'apps', 'utils']
+from bosos_files.home import App, HomeScreen
+from bosos_files.app_window import AppWindow
+from bosos_files.utils import (
+    APPS_PATH, 
+    get_app_and_class_name, 
+    setup_textures
+)
+
+__all__ = [
+    "APPS_PATH",
+    "App",
+    "HomeScreen",
+    "AppWindow",
+    "setup_textures",
+    "get_app_and_class_name",
+]
