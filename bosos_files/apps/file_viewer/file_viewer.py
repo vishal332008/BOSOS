b4ba39a073ff90253020a524c8befa858792fc55
shailesh_gabu_11 <116952795+Shailesh051211@users.noreply.github.com>
2025-03-23 17:28:01 +0530

    (Re)-structuring widgets (#7)
    * Add files via upload
    
    * Delete BOSOS.py
    
    * Delete bosos_files/app_window.py
    
    * Delete bosos_files/home.py
    
    * Add files via upload
    
    * Delete home.py
    
    * Add files via upload

--- bosos_files/apps/file_viewer/file_viewer.py
+++ bosos_files/apps/file_viewer/file_viewer.py
@@ -1,38 +1,26 @@
-# pylint: disable=all
 
 from __future__ import annotations
 
-from typing import override
-
-import babase
-import bauiv1 as bui
-import bascenev1 as bs
-from bauiv1lib.qrcode import QRCodeWindow
-from bosos_files.app_window import AppWindow
-
 import os
 import functools
+import bauiv1 as bui
+import bosos_files as bos
+
 
 # export app File_Viewer
-class FileViewerScreen(AppWindow):
+class FileViewerScreen(bos.AppWindow):
+
     def __init__(
         self,
         app_data,
-        width: float = 800.0,
-        height: float = 550.0,
-        transition: str | None = 'in_right',
         origin_widget: bui.Widget | None = None,
     ):
-        # pylint: disable=too-many-statements
 
         app = bui.app
         assert app.classic is not None
 
         super().__init__(
             app_data=app_data,
-            width=width,
-            height=height,
-            transition=transition,
             origin_widget=origin_widget,
         )
 
@@ -42,16 +30,17 @@ class FileViewerScreen(AppWindow):
         self._build_ui()
 
     def _build_ui(self) -> None:
+
         self._audio_button = bui.buttonwidget(
             parent=self._root_widget,
-            position=(self._width/2 - 180, self._height/2),
+            position=(self.width/2 - 180, self.height/2),
             size=(360, 120),
             label='Audio',
             text_scale=1.3,
         )
         self._texture_button = bui.buttonwidget(
             parent=self._root_widget,
-            position=(self._width/2 - 180, self._height/2 - 130),
+            position=(self.width/2 - 180, self.height/2 - 130),
             size=(360, 120),
             label='Textures',
             text_scale=1.3,
@@ -59,4 +48,5 @@ class FileViewerScreen(AppWindow):
         )
 
     def _open_textures_window(self):
+        
         print("open texture button pressed")
--- bosos_files/utils.py
+++ bosos_files/utils.py
@@ -1,21 +1,36 @@
-import babase
-import bauiv1 as bui
+
+from __future__ import annotations
+from dataclasses import dataclass
 
 import os
 import re
 import shutil
 
+import bauiv1 as bui
+
 CURRENT_DIR = os.path.dirname(__file__)
 APPS_PATH = CURRENT_DIR + os.sep + 'apps'
 
-def setup_textures():
+@dataclass
+class App:
+
+    name: str
+    texture: str
+    filename: str
+    classname: str
+    path: str
+
+
+def setup_textures() -> None:
+
     buiapp = bui.app
     assert buiapp.classic is not None
     
     apps = os.listdir(APPS_PATH)
+    texture_path = f"{os.getcwd()}{os.sep}ba_data{os.sep}textures2{os.sep}"
     for app in apps:
         files = os.listdir(APPS_PATH + os.sep + app)
-        if buiapp.classic.platform == 'android':
+        if bui.app.classic.platform == 'android':
             textures = [tex for tex in files if tex.endswith('.ktx')]
         else:
             textures = [tex for tex in files if tex.endswith('.dds')]
@@ -23,18 +38,20 @@ def setup_textures():
         if textures == []:
             print(f"Warning: No Textures found for {app} (app logo might be missing)")
             continue
-
-        path = f'{os.getcwd()}{os.sep}ba_data{os.sep}textures{os.sep}apps{os.sep}'
+        
         for tex in textures:
-            if not os.path.exists(path + app):
-                os.makedirs(path + app, exist_ok=True)
-            shutil.copy(
-                APPS_PATH + os.sep + app + os.sep + tex,
-                path + app + os.sep + tex
-            )
+            try:
+                if not os.path.exists(texture_path + tex):
+                    shutil.copy(
+                        APPS_PATH + os.sep + app + os.sep + tex,
+                        texture_path + os.sep + tex
+                    )
+            except Exception as e:
+                print(e)
 
 
 def get_app_and_class_name(path, filename) -> list[str] | None:
+
     pattern1 = re.compile(r'^class (\w+)\s*\(')
     pattern2 = re.compile(r'^# export app (\w+)$') # r'^#\s+export\s+app\s+(\w+)$'
     app_and_class = [0, 1]
@@ -51,7 +68,33 @@ def get_app_and_class_name(path, filename) -> list[str] | None:
                             f"\'# export app {app_and_class[0]}\' in {filename}.py"
                     )
                     return None
+                
             if pattern2.match(line):
                 class_line = True
                 app_and_class[0] = pattern2.match(line).group(1).replace('_', ' ')
+
         return None
+
+def load_apps() -> list[App]:
+    
+    apps: list[App] = []
+    folderlist = os.listdir(APPS_PATH)
+    for app in folderlist:
+        names = get_app_and_class_name(
+            path=(APPS_PATH + os.sep + app), filename=app
+        )
+        if names is not None:
+            appname = names[0].strip()
+            classname = names[1].strip()
+            if classname and appname:
+                apps.append(
+                    App(
+                        name=appname,
+                        texture="texture",
+                        filename=app,
+                        classname=classname,
+                        path=(APPS_PATH + os.sep + app)
+                    )
+                )
+    
+    return apps
